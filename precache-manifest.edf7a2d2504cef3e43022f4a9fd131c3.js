b4a6b04fb43ec1452c341ea21e9c007c73658dfa
Puneet Ahuja <puneet.ahuja@olx.com>
2020-07-17 03:28:08 +0530

    Updates

---
+++ precache-manifest.edf7a2d2504cef3e43022f4a9fd131c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497ab83d8cccdd24386885f482740bf9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5b07c5f121ad47862435",
+    "url": "/portfolio/static/css/main.1dc6f51f.chunk.css"
+  },
+  {
+    "revision": "6af3d869ce6ebc6bc825",
+    "url": "/portfolio/static/js/2.67ec76ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.67ec76ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b07c5f121ad47862435",
+    "url": "/portfolio/static/js/main.a8579396.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
