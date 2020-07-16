5644a5ea92d549128adb21d92c38f0bb269547ae
Puneet Ahuja <puneet.ahuja@olx.com>
2020-07-17 03:16:37 +0530

    Added responsiveness

--- src/components/Section/components/AboutSection/index.js
+++ src/components/Section/components/AboutSection/index.js
@@ -8,8 +8,8 @@ function AboutSection() {
     return (
         <div className="about-section" >
             <div className='about-section-heading'>Hi I'm Puneet.</div>
-            <div className='about-section-subheading'>Currently, I am working/learning at OLX India Pvt. Ltd. based in Gurgaon India.</div>
-            <div className='about-section-subheading'>I'm a frontend engineer with passion of writing scalable, easy-to-ubderstand abd clean-UIs.</div>
+            <div className='about-section-subheading'>I am currently working/learning at OLX India Pvt. Ltd. based in Gurgaon India.</div>
+            <div className='about-section-subheading'>I'm a frontend engineer with passion of writing scalable, easy-to-understand and clean-UIs.</div>
             {aboutDetails.map((details,index)=>{
                 const { title, data=[] } = details
                 return (
