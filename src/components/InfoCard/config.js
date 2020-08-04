05d4cc6a3638f7d40fa5b58b4a3b54ccf7853603
Puneet Ahuja <puneet.ahuja@olx.com>
2020-08-04 23:24:31 +0530

    Added Basic Tracking

--- src/components/InfoCard/config.js
+++ src/components/InfoCard/config.js
@@ -2,21 +2,25 @@ export const links = [
     {
         href: 'https://www.facebook.com/ahujapuneet92',
         iconUrl:'/assets/icons/facebook.svg',
-        alt:'Facebook'
+        alt:'Facebook',
+        name: 'Facebook'
     },
     {
         href: 'https://www.linkedin.com/in/devpuneetahuja/',
         iconUrl:'/assets/icons/linkedin.svg',
-        alt:'LinkedIn'
+        alt:'LinkedIn',
+        name: 'LinkedIn'
     },
     {
         href: 'https://www.youtube.com/channel/UCeZTAw0Gccxn8cowQSORyLg',
         iconUrl:'/assets/icons/youtube.svg',
-        alt:'Youtube'
+        alt:'Youtube',
+        name: 'Youtube'
     },
     {
         href: 'https://github.com/puneet-ahuja',
         iconUrl:'/assets/icons/github.svg',
-        alt:'GitHub'
+        alt:'GitHub',
+        name: 'GitHub'
     }
 ]
