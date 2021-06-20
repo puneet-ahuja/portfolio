803a4ad07ab566eff0094c10a29995ee2c08cf01
Puneet Ahuja <puneet.ahuja@olx.com>
2021-06-20 22:20:01 +0530

    Added Updated Youtube Experience.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # portfolio
 Code for portfolio
+
+
+# Local Setup.
+Run `yarn start` and test on localhost:3000
+
+# Deploy
+Run `yarn deploy` and verify changes on https://puneetahuja.in.
+**Note:** Always deploy from master.
--- src/components/Section/components/VideosSection/config.js
+++ src/components/Section/components/VideosSection/config.js
@@ -1,11 +1,26 @@
 export const videos = [
     {
-        title: 'React Lazy Image',
-        url: 'https://www.youtube.com/embed/sRcUqOjiPQw'
+        title: 'Implementation of React Window',
+        url: 'https://www.youtube.com/embed/5jiTVkTkPlw'
     },
     {
-        title: 'Javascript Map',
-        url: 'https://www.youtube.com/embed/gahGgJltAkE'
+        title: 'Chained Calculator',
+        url: 'https://www.youtube.com/embed/S4yfjshXWaE'
     },
-
+    {
+        title: 'Understanding setTimeout and setInterval',
+        url: 'https://www.youtube.com/embed/qSCEjJL3z6c'
+    },
+    {
+        title: 'Implementation of setInterval Polyfill',
+        url: 'https://www.youtube.com/embed/79yHvDzVA_I'
+    },
+    {
+        title: 'Implementation of Lazy Loading of Image in React',
+        url: 'https://www.youtube.com/embed/cfQR3f4eXv0'
+    },
+    {
+        title: 'Hoisting in Javascript',
+        url: 'https://www.youtube.com/embed/wLMKVV7jiPc'
+    },
 ]
